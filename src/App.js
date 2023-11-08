34cd82e467e11e9f4fe292df519e14151603d1b5
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-08 10:30:52 +0500

    rootes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.scss';
+import './scss/App.scss'
+import Navbar from './layout/navbar/Navbar';
 import RouteController from './routes';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <RouteController/>
     </>
   );
--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import './Navbar.scss'
 import { Container } from '../../utils'
+import logo from '../../assets/image/logo.svg'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <nav className='nav'>
-        <Container>
-            
-        </Container>
-    </nav>
-  )
+    return (
+        <nav className='nav'>
+            <Container>
+                <div className="nav__wrapper">
+                    <div className="nav__logo">
+                        <img width={200} height={50} src={logo} alt="" />
+                        <h1 className='nav__seo-title'>Medium</h1>
+                    </div>
+                    <ul className="nav__link">
+                        <li className="nav__item"><NavLink to='/'>Home</NavLink></li>
+                        <li className="nav__item"><NavLink to='/membership'>Membership</NavLink></li>
+                        <li className="nav__item"><NavLink to='articles'>Articles</NavLink></li>
+                        <li className="nav__item"><NavLink to='auth/login'>Login</NavLink></li>
+                        <li className="nav__item"><NavLink to='auth/signup'>Get Started</NavLink></li>
+                    </ul>
+                </div>
+            </Container>
+        </nav>
+    )
 }
 
 export default Navbar
