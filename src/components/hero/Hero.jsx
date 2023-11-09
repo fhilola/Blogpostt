f2b79cee7cc6a750253cb874e0b7aa3f35f21ce1
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-09 08:32:22 +0500

    hero fixed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,22 @@
+import { Button, Container } from '../../utils'
+import hero from '../../assets/image/hero.svg'
 import './Hero.scss'
 
 const Hero = () => {
   return (
     <div className='hero'>
-        
+      <Container>
+        <div className="hero__wrapper">
+          <div className="hero__content">
+        <h2>Stay curios.</h2>
+        <p>Discover stories, thinking, and expertise from writers on any topic</p>
+        <Button text={'Get started'}/>
+      </div>
+        <div className='hero__image'>
+          <img src={hero} alt="" />
+        </div>
+        </div>
+      </Container>
     </div>
   )
 }
