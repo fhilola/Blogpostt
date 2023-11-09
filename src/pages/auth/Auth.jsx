5dd63fcb56e0093f5f39a01e99b87f2aab3db069
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-09 10:22:58 +0500

    login and signup styled

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,9 +1,25 @@
-import {Outlet} from 'react-router-dom'
+import './Auth.scss'
+import {NavLink, Outlet} from 'react-router-dom'
 
 const Auth = () => {
   return (
-    <div>
-      <Outlet/>
+    <div className='auth'>
+      <div className='auth__container'>
+        <nav className="auth__nav">
+          <ul className='auth__nav-list'>
+            <li className='auth__nav-item'>
+              <NavLink to={'/auth/login'} className={({isActive}) => isActive ? 'auth__nav-link auth__nav-link--active' : 'auth__nav-link'}>Login</NavLink>
+            </li>
+            <li className='auth__nav-item'>
+              <NavLink to={'/auth/signup'} className={({isActive}) => isActive ? 'auth__nav-link auth__nav-link--active' : 'auth__nav-link'}>SignUp</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <form>
+        <Outlet/>
+      </form>
+      </div>
+      
     </div>
   )
 }
