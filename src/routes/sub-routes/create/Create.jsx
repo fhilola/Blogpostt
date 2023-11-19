073c9bbb9b8c76c1b0ee27ce5633b53876bc13bb
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-20 04:30:45 +0500

    react-quill created

--- src/routes/sub-routes/create/Create.jsx
+++ src/routes/sub-routes/create/Create.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Create = () => {
+  const [value, setValue] = useState('');
   return (
-    <div>Create</div>
+    <div>
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
+    </div>
   )
 }
 
